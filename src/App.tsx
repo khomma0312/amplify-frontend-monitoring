88cf7993dd7b561a0013e0d60d700dbc0e05913b
khomma0312 <kijikuroneko@gmail.com>
2024-08-16 12:13:12 +0900

    CloudWatch RUMを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { AwsRum, AwsRumConfig } from "aws-rum-web";
+
+try {
+  const config: AwsRumConfig = {
+    sessionSampleRate: 1,
+    identityPoolId: "ap-northeast-1:e10f7e44-85e1-4d5e-b1b6-18b68a4345b3",
+    endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com",
+    telemetries: ["performance", "errors", "http"],
+    allowCookies: true,
+    enableXRay: false,
+  };
+
+  const APPLICATION_ID: string = "ae20ca01-4185-4a04-9840-c2a64cbcd5c7";
+  const APPLICATION_VERSION: string = "1.0.0";
+  const APPLICATION_REGION: string = "ap-northeast-1";
+
+  const awsRum: AwsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +55,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
